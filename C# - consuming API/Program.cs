5e1e9ebe9ec3bf2833c416ede187fafae4b647d4
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-24 19:18:33 -0300

    3-commit

--- C# - consuming API/Program.cs
+++ C# - consuming API/Program.cs
@@ -1,13 +1,56 @@
 ﻿using (HttpClient client = new HttpClient())
 {
+    #region Principal
+
+    //try
+    //{
+    //    string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+    //    Console.WriteLine(response);
+    //}
+    //catch (Exception ex) {
+    //    Console.WriteLine(ex);
+    //}
+
+    #endregion
+
+    #region
+
+    //try
+    //{
+    //    for (int i = 3; i >= 0; i--)
+    //    {
+    //        Console.WriteLine($"{10 / i}");
+    //    }
+    //}
+    //catch (Exception ex)
+    //{
+    //    Console.WriteLine($"Tivemos um problema: {ex.Message}");
+    //}
+
+    #endregion
+
+    #region Primeiro Desafios
+
+    #region 1
+
     try
     {
-        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        string response = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
 
         Console.WriteLine(response);
     }
-    catch (Exception ex) {
+    catch (Exception ex)
+    {
         Console.WriteLine(ex);
     }
+
+    #endregion
+
+    #region 2
+
+    #endregion
+
+    #endregion
 }
-        
+
