b6c4215331d2a41a798fdf88fbe8b1821a72319f
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-23 22:47:01 -0300

    1-commit

--- C# - consuming API/Program.cs
+++ C# - consuming API/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using (HttpClient client = new HttpClient())
+{
+    string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+    Console.WriteLine(response);
+}
+        
